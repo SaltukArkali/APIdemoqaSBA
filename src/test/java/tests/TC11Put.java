c90cdcd1d9718ae78ba022fe1f0ab33ac5a751fb
Saltuk <saltukarkali@gmail.com>
2021-02-10 01:57:28 +0100

    local saltuk sba

--- src/test/java/tests/TC11Put.java
+++ src/test/java/tests/TC11Put.java
@@ -5,9 +5,7 @@ import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
 import java.util.*;
-
 import static io.restassured.RestAssured.*;
 
 public class TC11Put {
@@ -78,6 +76,16 @@ public class TC11Put {
         return response;
     }
 
+    public Response getUserBooks(){
+        String getEntpoint = "https://demoqa.com/Account/v1/User/0918fbb4-0298-4cbc-a462-ad5ffc823dc5";
+        response = given().
+                contentType(ContentType.JSON).
+                auth().preemptive().basic(userName,password).
+                when().
+                get(getEntpoint);
+        return response;
+    }
+
     @Test
     public void TC1101() {
         /*"1.  Kullanici adi  ""ozkan1"" ve sifresi ""JQ3iPpTEKTLjSQJ!"" olan
@@ -100,40 +108,78 @@ public class TC11Put {
 
         System.out.println("sonra :" + jsonPath.getList("books.isbn"));
     }
-//
-//    @Test
-//    public void TC1102(){
-//        /*2. Kullanici adi  ""ozkan1"" ve sifresi ""JQ3iPpTEKTLjSQJ!"" olan
-//         * kullanicinin ""9781593277574"" isbn li kitabin kullanicinin listesinde oldugunu dogrulayiniz.
-//         * kullanicinin kitaplarindan ""9781593277574""  isbn li kitabi ""9781491950296"" isbnli kitap ile degistiriniz.
-//         * Status kodunun un 200 oldugunu dogrulayiniz.
-//         * ""9781593277574"" isbn li kitabin kullanicin listesinde artik  olmadigini dogrulayiniz.
-//          Bug bulduysaniz sebebini aciklayiniz. (1. Test caseden farkini aciklayiniz)         /
-//
-//
-//
-//    }
-//
-//    @Test
-//    public void TC1103(){
-//        /*3. Kullanici adi  ""ozkan1"" ve sifresi ""JQ3iPpTEKTLjSQJ!"" olan
-//         * kullanicinin kitaplarindan ""9781449331818"" isbn li kitabi 9781449337711 (integer olarak giriniz) isbnli
-//         * kitap ile guncelleyiniz.
-//         * response un ""application/json"" tipinde oldugunu
-//          Status kodunun un 400 oldugunu dogrulayiniz."        /
-//
-//
-//
-//    }
-//
-//    @Test
-//    public void TC1104(){
-//        /*4. Kullanici adi  ""ozkan1"" ve sifresi ""WrNg!Def123KZW"" olan
-//         * kullanicinin kitaplarindan ""9781449331818"" isbn li kitabi 9781449337711 (integer) isbnli kitap ile guncelleyiniz
-//         * Status kodunun un 401  oldugunu
-//          Responstaki hata mesajinin   ""User not authorized!"" ve hata kodunun ""1200"" oldugunu dogrulayiniz."   /
-//
-//
-//
-//    }
+
+    @Test
+    public void TC1102(){
+        /*2. Kullanici adi  ""ozkan1"" ve sifresi ""JQ3iPpTEKTLjSQJ!"" olan
+         * kullanicinin ""9781593277574"" isbn li kitabin kullanicinin listesinde oldugunu dogrulayiniz.
+         * kullanicinin kitaplarindan ""9781593277574""  isbn li kitabi ""9781491950296"" isbnli kitap ile degistiriniz.
+         * Status kodunun un 200 oldugunu dogrulayiniz.
+         * ""9781593277574"" isbn li kitabin kullanicin listesinde artik  olmadigini dogrulayiniz.
+          Bug bulduysaniz sebebini aciklayiniz. (1. Test caseden farkini aciklayiniz)         /
+        getUserBooks();*/
+
+        jsonPath = response.jsonPath();
+        System.out.println("Once : "+ jsonPath.getList("books.isbn"));
+        Assert.assertTrue(jsonPath.getList("books.isbn").contains("9781593277574"));
+
+        putMethod(userName,password,"9781491950296","9781593277574");
+        jsonPath = response.jsonPath();
+        System.out.println("Sonra : "+ jsonPath.getList("books.isbn"));
+
+        response.
+                then().
+                assertThat().
+                statusCode(200);
+        Assert.assertFalse(jsonPath.getList("books.isbn").contains("9781593277574"));
+
+    }
+
+    @Test
+    public void TC1103(){
+        /*3. Kullanici adi  ""ozkan1"" ve sifresi ""JQ3iPpTEKTLjSQJ!"" olan
+         * kullanicinin kitaplarindan ""9781449331818"" isbn li kitabi 9781449337711 (integer olarak giriniz) isbnli
+         * kitap ile guncelleyiniz.
+         * response un ""application/json"" tipinde oldugunu
+          Status kodunun un 400 oldugunu dogrulayiniz."        */
+
+        getUserBooks();
+
+        jsonPath = response.jsonPath();
+        System.out.println("TC03 mevcut Liste :"+ jsonPath.getList("books.isbn"));
+
+        putMethod(userName,password,9781449337711.0,"9781449331818");
+
+        response.
+                then().
+                assertThat().
+                statusCode(400).
+                contentType("application/json");
+
+    }
+
+    @Test
+    public void TC1104(){
+        /*4. Kullanici adi  ""ozkan1"" ve sifresi ""WrNg!Def123KZW"" olan
+         * kullanicinin kitaplarindan ""9781449331818"" isbn li kitabi 9781449337711 (integer) isbnli kitap ile guncelleyiniz
+         * Status kodunun un 401  oldugunu
+          Responstaki hata mesajinin   ""User not authorized!"" ve hata kodunun ""1200"" oldugunu dogrulayiniz."   */
+
+    getUserBooks();
+
+        jsonPath = response.jsonPath();
+
+        String wrongPassword = "WrNg!Def123KZW";
+        putMethod(userName,wrongPassword,9781449337711l,"9781449331818");
+        response.
+                then().
+                assertThat().
+                statusCode(401);
+
+        jsonPath = response.jsonPath();
+
+        Assert.assertEquals(jsonPath.getString("code"),"1200");
+        Assert.assertEquals(jsonPath.getString("message"),"User not authorized!");
+
+    }
 }
